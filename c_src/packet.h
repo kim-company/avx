e6522850a169b597ceccaadebedd2b195d09e11e
Daniel Morandini <danielmorandini@me.com>
2023-09-29 09:07:40 +0200

    Revert "Split c files (attempt)"
    This reverts commit 4160b9bef90d6e95bbe0e86fa00d9a0db8241fb5.

--- c_src/packet.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PACKET_H
-#define _PACKET_H
-
-#include "nif.h"
-
-void load_packet_resources(ErlNifEnv *env);
-
-#endif
--- lib/avx.ex
+++ lib/avx.ex
@@ -2,4 +2,17 @@ defmodule AVx do
   @moduledoc """
   Documentation for `AVx`.
   """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AVx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
 end
