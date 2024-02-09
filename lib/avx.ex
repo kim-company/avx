a1e3930d27b702e97bd3dbf9c5ab452119a288fb
Daniel Morandini <danielmorandini@me.com>
2024-02-09 12:02:35 +0100

    Add utility function

--- lib/avx.ex
+++ lib/avx.ex
@@ -1,2 +1,6 @@
 defmodule AVx do
+  @spec pts_to_secs(pos_integer(), AVx.Demuxer.stream()) :: float()
+  def pts_to_secs(pts, stream) do
+    pts * stream.timebase_num / stream.timebase_den
+  end
 end
